b87c654de809004890d1fc9ae0905f9226ec0c0b
VolodymyK <114539981+VolodymyK@users.noreply.github.com>
2023-04-11 17:40:25 -0500

    a

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,69 +1,51 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
-// Масив зображень
-const galleryItems = [
-  {
-    preview: 'img/preview-1.jpg',
-    fullview: 'img/fullview-1.jpg',
-    description: 'Description 1',
-  },
-  {
-    preview: 'img/preview-2.jpg',
-    fullview: 'img/fullview-2.jpg',
-    description: 'Description 2',
-  },
-  {
-    preview: 'img/preview-3.jpg',
-    fullview: 'img/fullview-3.jpg',
-    description: 'Description 3',
-  },
-];
+const galleryList = document.querySelector(".gallery");
 
-// Створення розмітки для кожного елемента галереї
-function createGalleryItemMarkup(item) {
-  return `
-    <li class="gallery__item">
-      <a href="#" class="gallery__link">
-        <img class="gallery__image" src="${item.preview}" data-source="${item.fullview}" alt="${item.description}">
-      </a>
-    </li>
-  `;
-}
+// Create and render gallery items
+const createGalleryItem = ({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+    </a>
+  </li>
+`;
 
-// Створення розмітки всієї галереї
-function renderGalleryItems() {
-  const galleryList = document.querySelector('.gallery');
-  const galleryMarkup = galleryItems.map(item => createGalleryItemMarkup(item)).join('');
-  galleryList.innerHTML = galleryMarkup;
-}
+galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
 
-// Відкриття модального вікна з повнорозмірним зображенням
-function openModal(event) {
+// Open lightbox on image click
+const onGalleryItemClick = (event) => {
   event.preventDefault();
-  if (event.target.nodeName !== 'IMG') return;
-  const fullviewUrl = event.target.dataset.source;
-  const instance = basicLightbox.create(`
-    <img src="${fullviewUrl}" width="800" height="600">
-  `);
-  instance.show();
+  if (event.target.nodeName !== "IMG") return;
 
-  // Додавання слухача події keydown для закриття модального вікна по клавіші Escape
-  document.addEventListener('keydown', closeModalOnEsc);
-}
+  const imgSrc = event.target.dataset.source;
+  const imgAlt = event.target.alt;
+  const lightboxImg = document.querySelector(".lightbox__image");
 
-// Закриття модального вікна по клавіші Escape
-function closeModalOnEsc(event) {
-  if (event.code === 'Escape') {
-    basicLightbox.close();
-    document.removeEventListener('keydown', closeModalOnEsc);
+  lightboxImg.src = imgSrc;
+  lightboxImg.alt = imgAlt;
+
+  const lightbox = basicLightbox.create(document.querySelector(".lightbox"), {
+    onClose: () => {
+      lightboxImg.src = "";
+      lightboxImg.alt = "";
+    },
+  });
+
+  lightbox.show();
+};
+
+galleryList.addEventListener("click", onGalleryItemClick);
+
+// Close lightbox on ESC press
+const onEscPress = (event) => {
+  const ESC_KEYCODE = "Escape";
+  if (event.code === ESC_KEYCODE) {
+    const lightbox = basicLightbox.get();
+    if (lightbox.visible()) {
+      lightbox.close();
+    }
   }
-}
+};
 
-// Рендеринг галереї
-renderGalleryItems();
-
-// Додавання слухача події кліку на ul.gallery
-const galleryList = document.querySelector('.gallery');
-galleryList.addEventListener('click', openModal);
+document.addEventListener("keydown", onEscPress);
