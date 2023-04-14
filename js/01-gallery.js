26581618acc18f0eaf591a6006cf3778e369ac37
VolodymyK <114539981+VolodymyK@users.noreply.github.com>
2023-04-14 16:20:47 -0500

    YESSSSSSSSSSSSSSSSSSSSSSSS

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,7 +5,7 @@ const galleryList = document.querySelector(".gallery");
 // Create and render gallery items
 const createGalleryItem = ({ preview, original, description }) => `
   <li class="gallery__item">
-    <a class="gallery__link" href="${original}">
+    <a class="gallery__link" href="${original}" data-original-img=${original}>
       <img class="gallery__image" src="${preview}" alt="${description}" />
     </a>
   </li>
@@ -13,26 +13,21 @@ const createGalleryItem = ({ preview, original, description }) => `
 
 galleryList.innerHTML = galleryItems.map(createGalleryItem).join("");
 
-// Initialize BasicLightbox for each image
-const instances = galleryItems.map(({ original }) =>
-  basicLightbox.create(`
-    <img src="${original}" width="800" height="600">
-  `)
-);
+// Store instances in an array
+const instances = [];
 
 // Show corresponding instance when image is clicked
 galleryList.addEventListener("click", (event) => {
   event.preventDefault();
-  const { target } = event;
-  if (target.classList.contains("gallery__image")) {
-    const index = Array.from(target.parentNode.parentNode.children).indexOf(
-      target.parentNode
-    );
-    instances[index].show();
-    document.addEventListener("keydown", (event) =>
-      onEscPress(event, instances[index])
-    );
-  }
+  const original = event.target
+    .closest(".gallery__link")
+    .getAttribute("data-original-img");
+  const instance = basicLightbox.create(`
+    <img src="${original}" width="800" height="600">
+  `);
+  instances.push(instance); // Add instance to array
+  instance.show();
+  document.addEventListener("keydown", (event) => onEscPress(event, instance));
 });
 
 // Close instance on Esc key press
@@ -40,6 +35,7 @@ const onEscPress = (event, instance) => {
   const ESC_KEYCODE = "Escape";
   if (event.code === ESC_KEYCODE) {
     instance.close();
+    instances.splice(instances.indexOf(instance), 1); // Remove instance from array
     document.removeEventListener("keydown", (event) =>
       onEscPress(event, instance)
     );
